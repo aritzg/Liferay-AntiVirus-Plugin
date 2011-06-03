10af73860156561d793464e2e37e1fbb515e7bd3
tapaspatra <tapas.patra1@gmail.com>
2011-06-03 03:33:49 -0700

    addFileEntry(long userId, long groupId, long folderId, String name, String title, String description, String changeLog,String extraSettings, File file, ServiceContext serviceContext) method has been modefied with virus scancode.

--- Liferay-AntiVirus-hook/docroot/WEB-INF/src/com/liferay/portlet/documentlibrary/service/DLFileEntryAntiVirusLocalService.java
+++ Liferay-AntiVirus-hook/docroot/WEB-INF/src/com/liferay/portlet/documentlibrary/service/DLFileEntryAntiVirusLocalService.java
@@ -1,14 +1,27 @@
 package com.liferay.portlet.documentlibrary.service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalService;
+import com.philvarner.clamavj.ClamScan;
+import com.philvarner.clamavj.ScanResult;
 
 public class DLFileEntryAntiVirusLocalService extends DLFileEntryLocalServiceWrapper{
+
+        private static final int PORT_NUMBER = 3310;
+	private static final int TIME_OUT = 60;
+	private static final String VIRUS_SCAN_STATUS_SUCESS = "PASSED";
+	private static final String VIRUS_SCAN_STATUS_FAILURE = "FAILED";
+	private static final String VIRUS_SCAN_STATUS_ERROR = "ERROR";
 
 	public DLFileEntryAntiVirusLocalService(
 			DLFileEntryLocalService dlFileEntryLocalService) {
@@ -37,9 +50,45 @@ public class DLFileEntryAntiVirusLocalService extends DLFileEntryLocalServiceWra
 			String name, String title, String description, String changeLog,
 			String extraSettings, File file, ServiceContext serviceContext)
 			throws PortalException, SystemException {
+
 		// TODO Auto-generated method stub
-		return super.addFileEntry(userId, groupId, folderId, name, title, description,
-				changeLog, extraSettings, file, serviceContext);
+
+		DLFileEntry dlFileEntry = null;
+		
+		try {
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			ClamScan clamScan = new ClamScan(inetAddress.getHostAddress(), PORT_NUMBER, TIME_OUT);
+			InputStream inputStream;
+			
+			try {
+				inputStream = new FileInputStream(file);
+				ScanResult scanResult =  clamScan.scan(inputStream);
+				
+				if(scanResult.getStatus().toString().equals(VIRUS_SCAN_STATUS_SUCESS)){
+					dlFileEntry = super.addFileEntry(userId, groupId, folderId, name, title, description,
+							changeLog, extraSettings, file, serviceContext);
+					//System.out.println("Scanned sucess fully..");
+				}else if(scanResult.getStatus().toString().equals(VIRUS_SCAN_STATUS_FAILURE)){
+					
+					File virusFile = new File("");
+					dlFileEntry = super.addFileEntry(userId, groupId, folderId, name, title, description,
+							changeLog, extraSettings, virusFile, serviceContext);
+					
+					//System.out.println("Virus detected!!!");
+				}else{
+					//System.out.println("Error !!");
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return dlFileEntry;
 	}
 	
 	@Override
